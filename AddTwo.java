b440a0a1d814113cfabe481e8f1b07bbfb18ec46
Shoval Biadgilin <154275595+ShovalBiadgilin@users.noreply.github.com>
2023-12-22 01:57:57 +0200

    Update7 AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,8 @@
 public class AddTwo{
-public static void main (String args[]){ 
-int a= Integer.parseInt(args[0]);
-int b=Integer.parseInt(args[1]);
-System.out.println(a+"+"+b+" = "+ (a+b));
+ public static void main (String args[]){
+ int a= Integer.parseInt(args[0]);
+ int b=Integer.parseInt(args[1]);
+
+ System.out.println(a+"+"+b+" = "+ (a+b));
 }
 }
-		 
