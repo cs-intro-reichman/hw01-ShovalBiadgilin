c30ada81693ef618ba1bd5a8bd7fe3aa91a706e9
ShovalBiadgilin <Shoval.biadgilin@post.runi.ac.il>
2023-12-21 23:33:36 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,22 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Gen3{
+ public static void main (String[] args){
+
+  int a= Integer.parseInt(args[0]);
+  int b= Integer.parseInt(args [1]);
+
+  int num=(int) (Math.random()*(b-a)+a);
+  int num2= (int) (Math.random()*(b-a)+a);
+  int num3= (int) (Math.random()*(b-a)+a);
+ 
+  int min1=Math.min (num, num2);
+  int min2=Math.min(min1,num3);
+ 
+  System.out.println(num);
+  System.out.println(num2);
+  System.out.println(num3);
+  System.out.println("The minimal generated number was "+ min2);
+  } 
 }
--- Triangle.java
+++ Triangle.java
@@ -3,8 +3,16 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
-}
+public class Triangle{ 
+ public static void main (String args[]){ 
+
+     int a= Integer.parseInt(args[0]);
+     int b= Integer.parseInt(args[1]);
+     int c= Integer.parseInt(args[2]);
+     boolean isGreater= ((a+b)>c && (a+c)>b&& (c+b)>a);
+	
+System.out.println(a+", "+b+", "+c+" : "+ isGreater);
+   
+     } 
+   } 
+  
